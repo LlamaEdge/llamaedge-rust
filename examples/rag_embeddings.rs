df8495153a47e2cb2dc843a2f222ec71473669d8
Xin Liu <sam@secondstate.io>
2025-01-07 16:59:18 +0800

    feat: new examples for embeddings
    Signed-off-by: Xin Liu <sam@secondstate.io>

---
+++ examples/rag_embeddings.rs
@@ -0,0 +1,16 @@
+use llamaedge::{params::EmbeddingsParams, Client};
+
+#[tokio::main]
+async fn main() {
+    const SERVER_BASE_URL: &str = "http://localhost:10086";
+
+    let client = Client::new(SERVER_BASE_URL).unwrap();
+
+    match client
+        .embeddings("Hello, world!".into(), EmbeddingsParams::default())
+        .await
+    {
+        Ok(embeddings) => println!("{:#?}", embeddings),
+        Err(e) => println!("Error: {}", e),
+    }
+}
